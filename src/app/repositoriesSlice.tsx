98daa976fa2347e4a196e7af4cc183b32f53c994
Gasay-Liza <gasayliz@yandex.ru>
2023-11-14 16:20:14 +0300

    add build

--- src/app/repositoriesSlice.tsx
+++ src/app/repositoriesSlice.tsx
@@ -2,7 +2,7 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { initialState, IRepository,  QueryVariables, IEdge } from '../utils/types';
 
-const secretKey = process.env.GITHUB_TOKEN;
+const {REACT_APP_GITHUB_TOKEN} = process.env;
 // GraphQL запрос для получения данных о репозиториях Github
 const query = `
 query ($query: String!, $first: Int, $last: Int, $after: String, $before: String, ){
@@ -51,7 +51,7 @@ export const fetchPublicRepositories = createAsyncThunk<IRepository, QueryVariab
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
-          'Authorization': `Bearer ${secretKey}`
+          'Authorization': `Bearer ${REACT_APP_GITHUB_TOKEN}`
         },
         body: JSON.stringify({ query, variables })
       })
